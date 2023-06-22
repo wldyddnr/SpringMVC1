20502cdea8f5e649e68e6997c9ddc8969ec539af
yonguk <wldyddnr@gmail.com>
2023-06-23 02:36:45 +0900

    frontcontrollerV3

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -2,7 +2,6 @@ package hello.servlet.web.frontcontroller;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
